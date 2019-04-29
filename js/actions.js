05a409c1dd42c0efbbb8d634d7df723131c5678f
Draaakos <orlando.andaur.c@gmail.com>
2019-04-28 21:43:00 -0400

    improvised

---
+++ js/actions.js
@@ -0,0 +1,34 @@
+let current = -1
+
+function next(number) {
+    if(number === 3) {
+        return
+    }
+
+    const listIframes = document.querySelectorAll(".embed-responsive-item");
+    const item =  listIframes[number]
+
+    for(i = 0; i < listIframes.length; i++) {
+        listIframes[i].classList.remove("enabled");  
+    }
+    
+    item.classList.add("enabled")
+    current = current + 1;
+}
+
+function back(number) {
+    if(number === -1) {
+        return
+    }
+
+    const listIframes = document.querySelectorAll(".embed-responsive-item");
+    const item =  listIframes[number]
+
+    for(i = 0; i < listIframes.length; i++) {
+        listIframes[i].classList.remove("enabled");  
+    }
+    
+    item.classList.add("enabled")
+    current = current - 1;
+}
+
